0a41061210bcf4cb5a12bb9d30b8ea6114959c5e
Kabir <skaal1399@gmail.com>
2026-05-20 21:13:05 +0530

    Delete frontend directory

--- frontend/src/utils/wallet.js
+++
@@ -1,5 +0,0 @@
-export const connectWallet = async () => {
-  if (!window.ethereum) throw new Error('MetaMask not installed');
-  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-  return accounts[0];
-};
